ab2e2c48f8076cc76c5fa9b668f0ac9dd26b2b05
C1tad31 <unitedmilitaryum@gmail.com>
2021-09-10 01:46:32 -0700

    first commit

---
+++ WhiteLightWebsite/src/main/java/com/whitelightapp/whitelightwebsite/WhiteLightWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.whitelightapp.whitelightwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteLightWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhiteLightWebsiteApplication.class, args);
+    }
+
+}
---
+++ WhiteLightWebsite/src/test/java/com/whitelightapp/whitelightwebsite/WhiteLightWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whitelightapp.whitelightwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteLightWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
